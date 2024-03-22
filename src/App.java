84a50e76f50f98c0e96ed88af0bc092b9a72fb95
João Igor <joaoigor21052004@gmail.com>
2024-03-22 15:57:50 -0300

    MainMemory Stores Real Values
    A lot to work on

--- src/App.java
+++ src/App.java
@@ -3,11 +3,11 @@ import java.util.*;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // Part 1 - Setting Up Memories
+        /*// Part 1 - Setting Up Memories
         System.out.println("Choose memory mapping type: ");
         System.out.println("1. Direct Mapping");
         System.out.println("2. Associative Mapping");
-        int mappingType = scanner.nextInt();
+        int mappingType = scanner.nextInt();*/
         System.out.println("Choose a value for MM in bytes: ");
         int mainMemorySizeBytes = scanner.nextInt();
         System.out.println("Choose a value for CACHE in bytes: ");
@@ -16,18 +16,41 @@ public class App {
         int blockSizeBytes = scanner.nextInt();
 
         // Output chosen mapping type
-        if (mappingType == 1) {
+        /*if (mappingType == 1) {
             System.out.println("Chosen mapping: Direct Mapping");
         } else if (mappingType == 2) {
             System.out.println("Chosen mapping: Associative Mapping");
-        }
+        }*/
 
         // Calculate parameters
+        //memória cache:
         int TAG = mainMemorySizeBytes / cacheSizeBytes;
-        int LINES = cacheSizeBytes / blockSizeBytes;
-        int tagBits = (int) (Math.log(TAG) / Math.log(2));
-        int lineBits = (int) (Math.log(LINES) / Math.log(2));
-        int wordBits = (int) (Math.log(blockSizeBytes) / Math.log(2));
+        //int LINES = cacheSizeBytes / blockSizeBytes;
+        //MEMORIA PRINCIPAL---------------------------------------------------------------
+        int linesMM = mainMemorySizeBytes / blockSizeBytes;
+        String[][][] mainM = new String[TAG][linesMM/blockSizeBytes][blockSizeBytes];
+        for(int i = 0; i < TAG; i++){
+            for(int j = 0; j < linesMM/TAG; j++){
+                for(int k = 0; k < blockSizeBytes; k++){
+                    mainM[i][j][k] = "0x" + Integer.toHexString((int)(Math.random()*1000));
+                }
+            }
+        }
+        for(int i = 0; i < TAG; i++){
+            System.out.println("TAG: " + i);
+            for(int j = 0; j < linesMM/TAG; j++){
+                for(int k = 0; k < blockSizeBytes; k++){
+                    System.out.print(mainM[i][j][k] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+        //----------------------------------------------------------------------
+
+        /*int tagBits = (int) (Math.log(TAG) / Math.log(2)); //tag in bits
+        int lineBits = (int) (Math.log(LINES) / Math.log(2));// line in bits
+        int wordBits = (int) (Math.log(blockSizeBytes) / Math.log(2)); //word in bits
 
         System.out.println("Number of bits used in TAG: " + tagBits);
         if (mappingType == 1) {
@@ -36,41 +59,18 @@ public class App {
             LINES = 0;
             System.out.println("Number of bits used in LINE: " + LINES);
         }
-        System.out.println("Number of bits used in WORD: " + wordBits);
+        System.out.println("Number of bits used in WORD: " + wordBits);*/
 
         // Part 2 - Cache Hit and Cache
         // Miss---------------------------------------------------------------------------------------
         // System.out.println("Enter sequence of addresses in hexadecimal (separated by
         // space): ");
 
-        // Initialize cache
-        int tagBitting = (int) Math.pow(2, tagBits);
-        int[][] tagBit = new int[tagBitting][];
-        int lineBit = (int) Math.pow(2, lineBits);
+        // TRANSFORM 
+        //int tagBit = (int) Math.pow(2, tagBits); // number of tags
+        //int lineBit = (int) Math.pow(2, lineBits); // number of lines
 
-        // Calculando o número de linhas para cada entrada em tagBit
-        int linesPerTag = lineBit / tagBitting;
-
-        // Inicializar tagBit e mainM dentro de tagBit
-        for (int i = 0; i < tagBitting; i++) {
-            tagBit[i] = new int[linesPerTag * blockSizeBytes];
-            representation(tagBit[i], linesPerTag, blockSizeBytes);
-        }
-        for (int i = 0; i < tagBit.length; i++) {
-            System.out.println("Tag " + i + ":");
-            for (int j = 0; j < tagBit[i].length; j++) {
-                System.out.print(tagBit[i][j] + " ");
-            }
-            System.out.println("\n--------------------");
-        }
         scanner.close();
     }
 
-    public static void representation(int[] argument, int rows, int cols) {
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                argument[i * cols + j] = (int) Math.round(Math.random() * 10);
-            }
-        }
-    }
 }
