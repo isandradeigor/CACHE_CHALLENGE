1eb9ddfda93a3983beb0d725f6912f09de6faa09
João Igor <joaoigor21052004@gmail.com>
2024-03-25 22:36:09 -0300

    Cache Presentation

--- src/App.java
+++ src/App.java
@@ -73,41 +73,45 @@ public class App {
     public static int searchCacheAndRam(int TAG, int linesMM, int blockSizeBytes, String[][][] mainM,
         String[][][] cacheM,
         int LINES, String[] inputValues) {
-        int cacheHit = 0;
-        for (String value : inputValues) {
-            boolean foundInCache = false;
-            // Verificando se o valor está na cache
-            for (int j = 0; j < LINES; j++) {
-                for (int k = 0; k < blockSizeBytes; k++) {
-                    if (cacheM[0][j][k].equals(value)) {
-                        foundInCache = true;
-                        cacheHit++;
-                        break;
-                    }
-                }
-                if (foundInCache) {
+    int cacheHit = 0;
+    for (String value : inputValues) {
+        boolean foundInCache = false;
+        // Verificando se o valor está na cache
+        for (int j = 0; j < LINES; j++) {
+            for (int k = 0; k < blockSizeBytes; k++) {
+                if (cacheM[0][j][k].equals(value)) {
+                    foundInCache = true;
+                    cacheHit++;
                     break;
                 }
             }
+            if (foundInCache) {
+                break;
+            }
+        }
 
-            if (!foundInCache) {
-                // Searching in MAIN MEMORY (RAM)
-                boolean foundInMainMemory = false;
-                for (int i = 0; i < TAG; i++) {
-                    for (int j = 0; j < linesMM / TAG; j++) {
-                        for (int k = 0; k < blockSizeBytes; k++) {
-                            if (mainM[i][j][k].equals(value)) {
-                                foundInMainMemory = true;
-                                // Copiando a linha inteira da memória principal para a cache
-                                for (int l = 0; l < LINES; l++) {
-                                    for (int m = 0; m < blockSizeBytes; m++) {
-                                        cacheM[0][l][m] = mainM[i][j][m];
-                                    }
+        if (!foundInCache) {
+            // Searching in MAIN MEMORY (RAM)
+            boolean foundInMainMemory = false;
+            for (int i = 0; i < TAG; i++) {
+                for (int j = 0; j < linesMM / TAG; j++) {
+                    for (int k = 0; k < blockSizeBytes; k++) {
+                        if (mainM[i][j][k].equals(value)) {
+                            foundInMainMemory = true;
+                            // Copiando a linha inteira da memória principal para a cache
+                            for (int l = 0; l < LINES; l++) {
+                                for (int m = 0; m < blockSizeBytes; m++) {
+                                    cacheM[0][l][m] = mainM[i][j][m];
                                 }
-                                break;
                             }
-                        }
-                        if (foundInMainMemory) {
+                            // Mostrando o estado atual da memória cache
+                            System.out.println("Atualização da cache:");
+                            for (String[] line : cacheM[0]) {
+                                for (String val : line) {
+                                    System.out.print(val + " ");
+                                }
+                                System.out.println();
+                            }
                             break;
                         }
                     }
@@ -115,10 +119,15 @@ public class App {
                         break;
                     }
                 }
+                if (foundInMainMemory) {
+                    break;
+                }
             }
         }
-        return cacheHit;
     }
+    return cacheHit;
+}
+
     public static int searchCacheAndRamAssociative(int TAG, int linesMM, int blockSizeBytes, String[][] mainM, String[][] cacheM,
             int linesCACHE, String[] inputValues) {
         int cacheHit = 0;
